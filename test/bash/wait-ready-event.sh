8186f57fa3a9ccc7075534faa973fe1b9976f350
Valentin “ThisIsMac” M <thisismac47@gmail.com>
2016-08-27 01:49:51 +0200

    (pm2) fix behavior when in launching state / rename test / fix behavior launching state in cluster mode

--- test/bash/wait-ready-event.sh
+++ test/bash/wait-ready-event.sh
@@ -7,40 +7,40 @@ cd $file_path/wait_ready_event
 
 ##### start with sending event and without waiting (fork mode)
 $pm2 start http-wait-start.js
-should 'should have started 1 apps' 'online' 1
+should 'should have started 1 app' 'online' 1
 $pm2 delete all
 
 ##### start with sending event and ask to wait (fork mode)
 $pm2 start http-wait-start.js --wait-ready
-should 'should have started 1 apps' 'online' 1
+should 'should have started 1 app' 'online' 1
 $pm2 delete all
 
 ##### start without sending event and without waiting (fork mode)
 $pm2 start http-wait-start.js
-should 'should have started 1 apps' 'online' 1
+should 'should have started 1 app' 'online' 1
 $pm2 delete all
 
 ##### start without sending event and ask to wait (fork mode)
-timeout 5 $pm2 start http-wait-start.js --wait-ready
-should 'should not have started apps' 'lauching' 1
+timeout 5 $pm2 start http-wait-start_nocb.js --wait-ready
+should 'should be in launcher state waiting for ready event' 'lauching' 1
 $pm2 delete all
 
 ##### start with sending event and without waiting (cluster mode)
 $pm2 start http-wait-start.js -i 2
-should 'should have started 1 apps' 'online' 1
+should 'should have started 1 app' 'online' 1
 $pm2 delete all
 
 ##### start with sending event and ask to wait (cluster mode)
 $pm2 start http-wait-start.js -i 2 --wait-ready
-should 'should have started 1 apps' 'online' 1
+should 'should have started 1 app' 'online' 1
 $pm2 delete all
 
 ##### start without sending event and without waiting (cluster mode)
 $pm2 start http-wait-start.js -i 2
-should 'should have started 1 apps' 'online' 1
+should 'should have started 1 app' 'online' 1
 $pm2 delete all
 
 ##### start without sending event and ask to wait (cluster mode)
-timeout 5 $pm2 start http-wait-start.js -i 2 --wait-ready
-should 'should not have started apps' 'lauching' 1
+timeout 5 $pm2 start http-wait-start_nocb.js -i 2 --wait-ready
+should 'should be in launcher state waiting for ready event' 'lauching' 1
 $pm2 delete all
